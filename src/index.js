68bf800a2e434fa52e5ecd9c857191c5528920f8
飯塚永治 <iidzukaeiji@MacBook-Pro.local>
2021-06-03 22:18:17 +0900

    learning JavaScript

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
-/**
- * const, let等の変数う宣言
- */
-var val1 = "var変数";
-console.log(val1);
+// const, let等の変数う宣言
+//var val1 = "var変数";
+//console.log(val1);
 
 // var変数は上書き可能
-val1 = "var変数を上書き";
-console.log(val1);
+//val1 = "var変数を上書き";
+//console.log(val1);
 
 // var変数は際宣言可能
-var val1 = "var変数を際宣言";
-console.log(val1);
+//var val1 = "var変数を際宣言";
+//console.log(val1);
+
+//let val2 = "let変数";
+//console.log(val2);
+
+// letは上書き可能
+//val2 = "let変数を上書き";
+//console.log(val2);
+
+// letは再宣言不可
+//let val2 = "let変数を際宣言";
+
+//const val3 = "const宣言";
+//console.log(val3);
+
+// constは上書き不可
+//val3 = "const変数を上書き";
+
+//const val3 = "際宣言";
+
+
+//const val4 = {
+//    name: "Ageha",
+//    age: 20,
+//};
+//
+//val4.name = "Nana";
+
+//console.log(val4);
+
+//const val5 = ['dog', 'cat'];
+//val5[0] = 'dag!';
+//val5.push("monkey");
+
+//console.log(val5);
